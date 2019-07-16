a536f8d8811bdae22e091a65727bf1a3e8748a6c
jlee7003 <50094911+jlee7003@users.noreply.github.com>
2019-07-17 00:44:00 +0900

    Update Q9.java

--- Practice1/BAEKJUN/CH3/Q9.java
+++ Practice1/BAEKJUN/CH3/Q9.java
@@ -1,10 +1,26 @@
 package CH3;
 import java.util.Scanner;
+
 public class Q9 {
-
 	public static void main(String[] args) {
-		Scanner sc =new Scanner(System.in);
-		int a=sc.nextInt();
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		for (int i = 1; i<=a; i++) 
+		{
+			System.out.println("*");
+		
+			if(i!=a)
+			{
+			for( int j = 1; j<=i; j++)
+			{
+				System.out.print("*");
+				
+			}
+			}
+			else
+				System.out.print("");
+			
+		
+		}
 	}
-
 }
