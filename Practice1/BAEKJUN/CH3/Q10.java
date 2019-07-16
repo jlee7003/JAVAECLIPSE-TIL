59cf4e8e283626fdb278c37dd56ae373074a0966
jlee7003 <50094911+jlee7003@users.noreply.github.com>
2019-07-17 00:46:12 +0900

    Update Q10.java

--- Practice1/BAEKJUN/CH3/Q10.java
+++ Practice1/BAEKJUN/CH3/Q10.java
@@ -1,10 +1,23 @@
 package CH3;
 import java.util.Scanner;
+
 public class Q10 {
-
 	public static void main(String[] args) {
-		Scanner sc =new Scanner(System.in);
-		int a=sc.nextInt();
-	}
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		for (int i = 1; i <= a; i++) {
+			for (int j = 1; j <= a-i; j++) {//a-1,a-2,a-3,a-4 만큼 공백 출력하기
+		      	System.out.print(" ");
+			}
+			for (int j = 1; j <= i; j++) {//행 i의 갯수 만큼 * 출력하기
+		      	System.out.print("*");
+				
+				
+			}
+			System.out.println(""); //행의 갯수 만큼 줄바꾸기
+			
+			
+		}
 
+	}
 }
