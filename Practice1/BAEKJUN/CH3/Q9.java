37670bb5dd1603e2d39b379654470d84fa8d60ce
jlee7003 <50094911+jlee7003@users.noreply.github.com>
2019-07-17 00:45:02 +0900

    Update Q9.java

--- Practice1/BAEKJUN/CH3/Q9.java
+++ Practice1/BAEKJUN/CH3/Q9.java
@@ -19,7 +19,32 @@ public class Q9 {
 			}
 			else
 				System.out.print("");
+	      /*
+	      
+	      
+	      import java.util.Scanner;
+
+public class 별찍기쉽게 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		for (int i = 1; i <= a; i++) {
 			
+			System.out.println("");
+		
+			for (int j = 0; j <i; j++) {
+				
+				System.out.print("*");
+				
+
+			}
+		}
+	}
+}
+	      
+	      
+	      
+	      */
 		
 		}
 	}
